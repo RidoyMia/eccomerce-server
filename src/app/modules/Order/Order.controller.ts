241300f67c6a85a5b6ca24e4fce9461c399f8c08
RidoyMia <ridoymia153159hero@gmail.com>
2023-10-25 06:49:56 -0600

    accesstoken

--- src/app/modules/Order/Order.controller.ts
+++ src/app/modules/Order/Order.controller.ts
@@ -27,7 +27,8 @@ const createOrderController = async(req:Request,res: Response,next:NextFunction)
 }
 const getAllOrderOfUserController = async(req:Request,res: Response,next:NextFunction) : Promise<Iorder[] | any> =>{
    try {
-    const accesstoken = req.header;
+    const {accesstoken} = req.headers;
+    
     //@ts-ignore
     const userInfo = await jwt.verify(accesstoken,config.ACCESSTOKEN as Secret)
     if(!userInfo){
